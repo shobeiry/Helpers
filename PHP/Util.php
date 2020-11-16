d26bff49967cc17295b5f6315bfbab83f16fe0b4
Seyed Sadeq Shobeiry <ssshobeiry@gmail.com>
2020-11-16 10:01:23 +0330

    Update Util.php
    - Util::is_ir_national_code(1111111111); added
    - Util::is_ir_mobile(09111111111); added

--- PHP/Util.php
+++ PHP/Util.php
@@ -21,4 +21,52 @@ class Util
         }
         return round($size, $precision) . ' ' . $units[$i];
     }
+
+    /**
+     * check input national code is iranian national code or not
+     *
+     * @example Util::is_ir_national_code(1111111111); // false
+     *
+     * @param $national_code
+     * @return boolean: check national code result
+     */
+    public static function is_ir_national_code($national_code)
+    {
+        $national_code = (string)preg_replace('/[^0-9]/', '', $national_code);
+
+        if (strlen($national_code) != 10) {
+            return false;
+        }
+
+        $list_code = str_split($national_code);
+        $last = (int)$list_code[9];
+        unset($list_code[9]);
+        $i = 10;
+        $sum = 0;
+
+        foreach ($list_code as $key => $_) {
+            $sum += intval($_) * $i--;
+        }
+
+        $mod = (int)$sum % 11;
+
+        if ($mod >= 2) {
+            $mod = 11 - $mod;
+        }
+
+        return $mod == $last;
+    }
+
+    /**
+     * check input number is iranian mobile number or not
+     *
+     * @example Util::is_ir_mobile(09111111111); // true
+     *
+     * @param $number
+     * @return boolean: check mobile number result
+     */
+    public static function is_ir_mobile($number)
+    {
+        return preg_match("/^(?:98|\+98|0098|0)?9[0-9]{9}$/", $number);
+    }
 }
