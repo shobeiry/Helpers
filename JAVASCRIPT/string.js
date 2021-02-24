45b899a82b343124ed135aaede82e4c491b3213d
SSSH <ssshobeiry@gmail.com>
2021-02-24 10:01:49 +0330

    Update string.js

--- JAVASCRIPT/string.js
+++ JAVASCRIPT/string.js
@@ -3,7 +3,7 @@ if (!String.prototype.format) {
         /**
          * string format
          *
-         * @example let temp = "hello {0}{1}.".format("wor", "ld") // temp == "hello world."
+         * @example let temp = "hello {0}{1}.".format("wor", "ld") // temp is "hello world."
          **/
         let args = arguments;
         return this.replace(/{(\d+)}/g, function (match, number) {
@@ -12,6 +12,50 @@ if (!String.prototype.format) {
                 : match
                 ;
         });
+    };
+}
+
+if (!String.prototype.f) {
+    String.prototype.f = function () {
+        /**
+         * string format
+         *
+         * @example let temp = "hello {0}{1}.".format("wor", "ld") // temp is "hello world."
+         **/
+        return this.format(arguments);
+    };
+}
+
+if (!String.prototype.isUUID) {
+    String.prototype.isUUID = function () {
+        /**
+         * check string is valid uuid
+         *
+         * @example let temp = "33ec9804-932d-48bf-87d7-175916830655".isUUID() // temp is true
+         **/
+        return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/.test(this);
+    };
+}
+
+if (!String.prototype.isIP) {
+    String.prototype.isIP = function () {
+        /**
+         * check string is valid ip
+         *
+         * @example let temp = "192.168.1.1".isIP() // temp is true
+         **/
+        return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(this);
+    };
+}
+
+if (!String.prototype.isEmail) {
+    String.prototype.isEmail = function () {
+        /**
+         * check string is valid email
+         *
+         * @example let temp = "example@info.com".isEmail() // temp is true
+         **/
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this);
     };
 }
 
